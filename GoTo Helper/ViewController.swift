e0f3a897912ee8a59d6b72414148f652f8a632aa
itslequid <dubin1917@ya.ru>
2019-08-14 10:36:01 +0300

    Initial Commit

---
+++ GoTo Helper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoTo Helper
+//
+//  Created by Георгий Александров on 14/08/2019.
+//  Copyright © 2019 Георгий Александров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
