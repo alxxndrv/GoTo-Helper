56ec19ea98c924b60f5c4808679c9f98ac340169
itslequid <dubin1917@ya.ru>
2019-08-16 00:36:02 +0300

    1st

---
+++ GoTo Helper/ScheduleCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ScheduleCollectionViewCell.swift
+//  GoTo Helper
+//
+//  Created by Георгий Александров on 16/08/2019.
+//  Copyright © 2019 Георгий Александров. All rights reserved.
+//
+
+import UIKit
+class ScheduleCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
--- GoTo Helper/ViewController.swift
+++ GoTo Helper/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    let reuseIdentifier = "cell"
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
 
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! ScheduleCollectionViewCell
+        cell.myLabel.text = self.items[indexPath.item]
+        return cell
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
 
 
